1ae03f700f9af9aa3a838f9816149f2038726f50
Alexey Evseev <evseev_alexey94@bk.ru>
2021-12-19 15:05:52 +0300

    fix: temporarly remove .env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,6 @@
 export const MOBILE_WIDTH = 320;
 export const TAB_WIDTH = 768;
 export const DESKTOP_WIDTH = 1280;
-export const { REACT_APP_MODE } = process.env;
 export const SHORT_MOVIES_DURATION = 40;
 export const FIRST_EXTRA_MOBILE = {first: 5, extra: 2};
 export const FIRST_EXTRA_TAB = {first: 8, extra: 2};
